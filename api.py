d27ec7b1e00d0c7c301404768e0bfec3032a052f
Bartek_Anczewski <s094217@student.tu.kielce.pl>
2023-06-29 19:29:31 +0200

    connected two function

--- api.py
+++ api.py
@@ -1,5 +1,7 @@
 import requests
 from datetime import datetime
+import Retrograde_weather
+
 
 def get_weather_forecast(lat, lon):
     user_api = 'ca7f97251a8e3e4fd7d10d6d94546ceb'  # Twój klucz API
@@ -58,90 +60,106 @@ def get_coordinates(location_name):
         return lat, lon
     
     return None, None
-
-
-def run_weather_app():
-    choose_language = input("What language? (English - E / Polish - P): ")
+def main_2():
     
-    #Function "lower" changes all letters to lowercase    
-    if choose_language.lower() == "e":
-        choose_ways = input("Search Name - N / Coordinates - C: ")
+    Retrograde_weather.main()
+    x = Retrograde_weather.main()
+    print(x)
+    
+def run_weather_app():
+    
+    choose_option = input("Currently weather? - C / Retrograde weather? - R ").lower()
+    
+    if choose_option == "r":   
+       
+       main_2()
         
-        if choose_ways.lower() == "n":
-            location_name = input("Enter city name:")
-            lat, lon = get_coordinates(location_name)
-        elif choose_ways.lower() == "c":
-            lat = input("Enter latitude (e.g., 52.2297):")
-            lon = input("Enter longitude (e.g., 21.0122):")
-        else:
-            print("Invalid selection.")
-            return
+    elif choose_option == "c":   
+        choose_language = input("What language? (English - E / Polish - P): ")
         
-        weather_info = get_weather_forecast(lat, lon)
-        location_name = get_location_name(lat, lon)
+    
+        #Function "lower" changes all letters to lowercase    
+        if choose_language.lower() == "e":
+            choose_ways = input("Search Name - N / Coordinates - C: ")
+        
+            if choose_ways.lower() == "n":
+                location_name = input("Enter city name:")
+                lat, lon = get_coordinates(location_name)
+            elif choose_ways.lower() == "c":
+                lat = input("Enter latitude (e.g., 52.2297):")
+                lon = input("Enter longitude (e.g., 21.0122):")
+            else:
+                print("Invalid selection.")
+                return
+        
+            weather_info = get_weather_forecast(lat, lon)
+            location_name = get_location_name(lat, lon)
 
-        if weather_info is not None and location_name is not None:
-            current_data = datetime.now().date()
-            print("---------------------",current_data,"--------------------------")
-            print("Weather for: {} || {}".format(location_name, weather_info['location']))
-            print("-------------------------------------------------------------")
-            print("Temperature: {:.2f} °C".format(weather_info['temperature']))
-            print("Weather description:", weather_info['weather_desc'])
-            print("Humidity: {}%".format(weather_info['humidity']))
-            print("Wind speed: {} km/h".format(weather_info['wind_speed']))
-            if weather_info is not None and location_name is not None:    #save to file txt
+            if weather_info is not None and location_name is not None:
+                current_data = datetime.now().date()
+                print("---------------------",current_data,"--------------------------")
+                print("Weather for: {} || {}".format(location_name, weather_info['location']))
+                print("-------------------------------------------------------------")
+                print("Temperature: {:.2f} °C".format(weather_info['temperature']))
+                print("Weather description:", weather_info['weather_desc'])
+                print("Humidity: {}%".format(weather_info['humidity']))
+                print("Wind speed: {} km/h".format(weather_info['wind_speed']))
+                if weather_info is not None and location_name is not None:    #save to file txt
                 
-                file_name = "Weather_"+("{}_".format(location_name)) + str(datetime.now().date()) +  ".txt" #name file is "Weather and current date"
+                    file_name = "Weather_"+("{}_".format(location_name)) + str(datetime.now().date()) +  ".txt" #name file is "Weather and current date"
                 
-                with open(file_name, "w") as file:
-                    file.write("Weather for: {} || {}\n".format(location_name, weather_info['location']))
-                    file.write("Temperature: {:.2f}°C\n".format(weather_info['temperature']))
-                    file.write("Humidity: {}%\n".format(weather_info['humidity']))
-                    file.write("Wind speed: {} km/h\n".format(weather_info['wind_speed']))
-            run_weather_app()
-        else:
-            print("Error fetching weather data or city name. Check the entered coordinates.")
-            run_weather_app()
+                    with open(file_name, "w") as file:
+                        file.write("Weather for: {} || {}\n".format(location_name, weather_info['location']))
+                        file.write("Temperature: {:.2f}°C\n".format(weather_info['temperature']))
+                        file.write("Humidity: {}%\n".format(weather_info['humidity']))
+                        file.write("Wind speed: {} km/h\n".format(weather_info['wind_speed']))
+                    run_weather_app()
+                else:
+                    print("Error fetching weather data or city name. Check the entered coordinates.")
+                    run_weather_app()
             
-    elif choose_language.lower() == "p":
-        choose_ways = input("Szukanie nazwą - N / Współrzędnymi - C: ")
+        elif choose_language.lower() == "p":
+            
+            choose_ways = input("Szukanie nazwą - N / Współrzędnymi - C: ")
         
-        if choose_ways.lower() == "n":
-            location_name = input("Wprowadź nazwę miejscowości:")
-            lat, lon = get_coordinates(location_name)
-        elif choose_ways.lower() == "c":
-            lat = input("Wprowadź szerokość geograficzną (np. 52.2297):")
-            lon = input("Wprowadź długość geograficzną (np. 21.0122):")
-        else:
-            print("Nieprawidłowy wybór.")
-            return
+            if choose_ways.lower() == "n":
+                location_name = input("Wprowadź nazwę miejscowości:")
+                lat, lon = get_coordinates(location_name)
+            elif choose_ways.lower() == "c":
+                lat = input("Wprowadź szerokość geograficzną (np. 52.2297):")
+                lon = input("Wprowadź długość geograficzną (np. 21.0122):")
+            else:
+                print("Nieprawidłowy wybór.")
+                return
         
-        weather_info = get_weather_forecast(lat, lon)
-        location_name = get_location_name(lat, lon)
+            weather_info = get_weather_forecast(lat, lon)
+            location_name = get_location_name(lat, lon)
 
-        if weather_info is not None and location_name is not None:   
-            print("-------------------------------------------------------------")
-            print("Pogoda dla {} || {}".format(location_name, weather_info['location']))
-            print("-------------------------------------------------------------")
-            print("Temperatura: {:.2f} °C".format(weather_info['temperature']))
-            print("Opis pogody:", weather_info['weather_desc'])
-            print("Wilgotność: {}%".format(weather_info['humidity']))
-            print("Prędkość wiatru: {} km/h".format(weather_info['wind_speed']))
-            if weather_info is not None and location_name is not None:   #save to file txt
+            if weather_info is not None and location_name is not None:   
+                print("-------------------------------------------------------------")
+                print("Pogoda dla {} || {}".format(location_name, weather_info['location']))
+                print("-------------------------------------------------------------")
+                print("Temperatura: {:.2f} °C".format(weather_info['temperature']))
+                print("Opis pogody:", weather_info['weather_desc'])
+                print("Wilgotność: {}%".format(weather_info['humidity']))
+                print("Prędkość wiatru: {} km/h".format(weather_info['wind_speed']))
+                if weather_info is not None and location_name is not None:   #save to file txt
                 
-                file_name = "Pogoda_"+("{}_".format(location_name)) + str(datetime.now().date()) +  ".txt"
-                with open(file_name, "w") as file:
-                    file.write("Pogoda dla {} || {}\n".format(location_name, weather_info['location']))
-                    file.write("Temperatura: {:.2f}°C\n".format(weather_info['temperature']))
-                    file.write("Wilgotność: {}%\n".format(weather_info['humidity']))
-                    file.write("Prędkość wiatru: {} km/h\n".format(weather_info['wind_speed']))
-            run_weather_app()
+                    file_name = "Pogoda_"+("{}_".format(location_name)) + str(datetime.now().date()) +  ".txt"
+                    with open(file_name, "w") as file:
+                        file.write("Pogoda dla {} || {}\n".format(location_name, weather_info['location']))
+                        file.write("Temperatura: {:.2f}°C\n".format(weather_info['temperature']))
+                        file.write("Wilgotność: {}%\n".format(weather_info['humidity']))
+                        file.write("Prędkość wiatru: {} km/h\n".format(weather_info['wind_speed']))
+                run_weather_app()
             
             
-        else:
-            print("Błąd w pobieraniu danych o pogodzie lub nazwy miejscowości. Sprawdź wprowadzone dane.")
+            else:
+                print("Błąd w pobieraniu danych o pogodzie lub nazwy miejscowości. Sprawdź wprowadzone dane.")
+                run_weather_app()
+        else: 
             run_weather_app()
-    else: 
+    else:
         run_weather_app()
     
 run_weather_app()
