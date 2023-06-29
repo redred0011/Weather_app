8a94c7a2d9db04fae11b1333b444f4afaad00d55
Bartek_Anczewski <s094217@student.tu.kielce.pl>
2023-06-29 21:29:35 +0200

    connected app

--- Weather_app.py
+++ Weather_app.py
@@ -1,6 +1,7 @@
 import requests
 from datetime import datetime
 import Retrograde_weather
+import Future_weather
 
 
 def get_weather_forecast(lat, lon):
@@ -68,9 +69,18 @@ def main_2(): #Funkcja pobiera dane z pliku projektu o nazwie "Retrograde_weathe
     x = Retrograde_weather.main()
     print(x)
     
+def main_3():
+    
+    Future_weather.main()
+    print(Future_weather.main())
+
 def run_weather_app(): #Główna funkcja programu Weather_app 
     
-    choose_option = input("Currently weather? - C / Retrograde weather? - R ").lower()  #Pobieranie danych od użytkownika 
+    choose_option = input("Currently weather? - C / Retrograde weather? - R / Future weather ? - F ").lower()  #Pobieranie danych od użytkownika 
+    
+    if choose_option == "f":
+        
+        main_3()
     
     if choose_option == "r":   
        
