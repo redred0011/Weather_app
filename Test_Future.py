d5249732852ee3e32f399e3d5b3129430a535dec
Bartek_Anczewski <s094217@student.tu.kielce.pl>
2023-07-01 19:52:40 +0200

    unit test Future_Weather

---
+++ Test_Future.py
@@ -0,0 +1,45 @@
+import requests
+from unittest import mock
+import pytest
+
+def get_future_weather(location, api_key):
+    unit_group = "metric"
+    url = f"https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/{location}?unitGroup={unit_group}&key={api_key}"
+    response = requests.get(url)
+    data = response.json()
+    return data
+
+def test_get_future_weather():
+    location = 'London'
+    api_key = 'H3BHSY2VSQD8XSZ3VD76RNDZM'
+
+    # Mock response data
+    mock_response = {
+        'days': [
+            {
+                'datetime': '2023-07-01',
+                'description': 'Sunny',
+                'tempmax': 25,
+                'tempmin': 18,
+                'windspeed': 10
+            },
+            {
+                'datetime': '2023-07-02',
+                'description': 'Cloudy',
+                'tempmax': 22,
+                'tempmin': 15,
+                'windspeed': 12
+            }
+        ]
+    }
+
+    with mock.patch('requests.get') as mock_get:
+        mock_get.return_value.json.return_value = mock_response
+
+        data = get_future_weather(location, api_key)
+
+        mock_get.assert_called_once_with(f"https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/{location}?unitGroup=metric&key={api_key}")
+        assert data == mock_response
+
+if __name__ == '__main__':
+    pytest.main()
