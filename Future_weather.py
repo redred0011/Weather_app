f9afa48f81dcd48282e05f260dfc2a4c18bd7ca0
Bartek_Anczewski <s094217@student.tu.kielce.pl>
2023-07-04 22:02:20 +0200

    added Sqlite3

--- Future_weather.py
+++ Future_weather.py
@@ -1,4 +1,5 @@
 import requests
+import sqlite3
 
 def get_future_weather(location, api_key):
     
@@ -7,38 +8,52 @@ def get_future_weather(location, api_key):
     response = requests.get(url)
     data = response.json()
     return data
-
+    
 def main():
+    connect = sqlite3.connect('Future_weather.db')
+    c = connect.cursor()
+
     choose_days = int(input("Podaj liczbę dni, z których chcesz otrzymać prognozę pogody - maks. 15:"))
 
     if 1 <= choose_days <= 15:
         location = input("Podaj lokalizację (np. London): ")
         api_key = 'H3BHSY2VSQD8XSZ3VD76RNDZM'
-        
+
         weather_data = get_future_weather(location, api_key)
 
-        if 'days' in weather_data:
-           
-            file_name =  "Pogoda_"+("{}".format(location))+".txt" 
-            with open(file_name, "w") as file:  
-                for index, day_data in enumerate(weather_data['days']):
-                    if index >= choose_days:
-                        break
-                    print("Data:", day_data['datetime'])
-                    print("Opis pogody:", day_data['description'])
-                    print("Temperatura maksymalna:", day_data['tempmax'])
-                    print("Temperatura minimalna:", day_data['tempmin'])
-                    print("Prędkość wiatru:", day_data['windspeed'])
-                    print("--------------------")
-                    file.write(f"Pogoda dla: {location}\n")  #Zapis do pliku txt
-                    file.write(f"Data: {day_data['datetime']}\n")
-                    file.write(f"Opis pogody: {day_data['description']}\n")
-                    file.write(f"Temperatura maksymalna: {day_data['tempmax']}\n")
-                    file.write(f"Temperatura minimalna: {day_data['tempmin']}\n")
-                    file.write(f"Prędkość wiatru: {day_data['windspeed']}\n")
-                    file.write("--------------------\n")
-        else:
-            print("Brak danych pogodowych dla podanej lokalizacji.")
+        # Sprawdzenie, czy tabela już istnieje
+        c.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='Weather'")
+        existing_table = c.fetchone()
+
+        if existing_table is None:
+            # Tworzenie tabeli, jeśli nie istnieje
+            c.execute('''CREATE TABLE Weather (id INTEGER PRIMARY KEY AUTOINCREMENT,
+                      Miejscowosc TEXT, 
+                      Data TEXT, Opis_pogody TEXT, 
+                      Max_temperatura REAL, 
+                      Min_temperatura REAL, 
+                      Predkosc_wiatru INTEGER)''')
+
+        # Wstawianie danych
+        for index, day_data in enumerate(weather_data['days']):
+            if index >= choose_days:
+                break
+            Miejscowosc = location
+            Data = day_data['datetime']
+            Opis_pogody = day_data['description']
+            Max_temperatura = day_data['tempmax']
+            Min_temperatura = day_data['tempmin']
+            Predkosc_wiatru = day_data['windspeed']
+            c.execute("INSERT INTO Weather (Miejscowosc, Data, Opis_pogody, Max_temperatura, Min_temperatura, Predkosc_wiatru) VALUES (?, ?, ?, ?, ?, ?)", (Miejscowosc, Data, Opis_pogody, Max_temperatura, Min_temperatura, Predkosc_wiatru))
+            
+        # Zapisywanie zmian
+        connect.commit()
+
+        # Zamykanie połączenia z bazą danych
+        connect.close()
+
+        print("Dane zostały dodane do bazy danych.")
     else:
         print("Wprowadzono nieodpowiednią liczbę dni. Spróbuj ponownie.")
 
+main()
